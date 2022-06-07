d4372b549fd7af91899954078fc4181ebbc823af
AlexandrBorovikov <borovikova481@gmail.com>
2022-06-07 10:19:05 +0300

    made page info shop

---
+++ src/pages/PageInfoShop.vue
@@ -0,0 +1,38 @@
+<template>
+  <Header />
+  <InfoShop />
+  <Footer />
+</template>
+
+<script>
+import InfoShop from '@/components/InfoShop.vue'
+import Header from '@/components/layout/Header.vue'
+import Footer from '@/components/layout/Footer.vue'
+  
+export default {
+  
+  components: {
+    InfoShop,
+    Header,
+    Footer
+  },
+  
+  data() {
+    return {
+
+    }
+  },
+}
+
+</script>
+
+<style lang="scss" scoped>
+  .greetings {
+
+    h1 {
+      color: $--color-dove-gray;
+      font-family: "trebuchetms";
+    }
+    
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../pages/HomeView.vue'
 import PageCatalog from '../pages/PageCatalog.vue'
+import PageInfoShop from '../pages/PageInfoShop.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/catalog',
       name: 'catalog',
       component: PageCatalog
+    },
+    {
+      path: '/info-shop',
+      name: 'info-shop',
+      component:  PageInfoShop
     }
   ]
 })
